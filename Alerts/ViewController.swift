5d507637a6f0d69dd621c37acb199f77fd3ab714
HeMet <hemet.mail@gmail.com>
2017-03-02 19:55:41 +0400

    Initial Commit

---
+++ Alerts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alerts
+//
+//  Created by Evgeniy Gubin on 02.03.17.
+//  Copyright © 2017 Evgeniy Gubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
